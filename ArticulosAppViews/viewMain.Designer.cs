5602bb9de65b2d40ba6f120bfe5fb8e7d57d73f0
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-09-11 13:16:52 -0300

    Actualicé la estetica de la ventana principal

--- ArticulosAppViews/viewMain.Designer.cs
+++ ArticulosAppViews/viewMain.Designer.cs
@@ -29,25 +29,25 @@
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.pictureBoxDecorationImage = new System.Windows.Forms.PictureBox();
             this.labelLogo = new System.Windows.Forms.Label();
             this.buttonSalir = new System.Windows.Forms.Button();
             this.buttonCategorias = new System.Windows.Forms.Button();
             this.buttonMarcas = new System.Windows.Forms.Button();
             this.buttonArticulos = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
+            this.pictureBoxDecorationImage = new System.Windows.Forms.PictureBox();
             this.panelWindowTitle = new System.Windows.Forms.Panel();
             this.labelWindowTitle = new System.Windows.Forms.Label();
             this.panelWindows = new System.Windows.Forms.Panel();
             this.panelMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDecorationImage)).BeginInit();
             this.panelWindowTitle.SuspendLayout();
+            this.panelWindows.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenu
             // 
-            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
-            this.panelMenu.Controls.Add(this.pictureBoxDecorationImage);
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.panelMenu.Controls.Add(this.labelLogo);
             this.panelMenu.Controls.Add(this.buttonSalir);
             this.panelMenu.Controls.Add(this.buttonCategorias);
@@ -55,35 +55,23 @@
             this.panelMenu.Controls.Add(this.buttonArticulos);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
-            this.panelMenu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(180, 537);
+            this.panelMenu.Size = new System.Drawing.Size(210, 578);
             this.panelMenu.TabIndex = 0;
-            // 
-            // pictureBoxDecorationImage
-            // 
-            this.pictureBoxDecorationImage.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBoxDecorationImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBoxDecorationImage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxDecorationImage.Image = global::ArticulosAppViews.Properties.Resources.logo1;
-            this.pictureBoxDecorationImage.Location = new System.Drawing.Point(0, 260);
-            this.pictureBoxDecorationImage.Name = "pictureBoxDecorationImage";
-            this.pictureBoxDecorationImage.Size = new System.Drawing.Size(180, 217);
-            this.pictureBoxDecorationImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxDecorationImage.TabIndex = 10;
-            this.pictureBoxDecorationImage.TabStop = false;
-            this.pictureBoxDecorationImage.UseWaitCursor = true;
             // 
             // labelLogo
             // 
             this.labelLogo.AutoSize = true;
             this.labelLogo.BackColor = System.Drawing.Color.Transparent;
-            this.labelLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogo.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelLogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.labelLogo.Location = new System.Drawing.Point(19, 27);
+            this.labelLogo.Location = new System.Drawing.Point(22, 29);
+            this.labelLogo.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelLogo.Name = "labelLogo";
-            this.labelLogo.Size = new System.Drawing.Size(141, 26);
+            this.labelLogo.Size = new System.Drawing.Size(150, 25);
             this.labelLogo.TabIndex = 0;
             this.labelLogo.Text = "Artículos App";
             this.labelLogo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -93,13 +81,13 @@
             this.buttonSalir.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.buttonSalir.FlatAppearance.BorderSize = 0;
             this.buttonSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSalir.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSalir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.buttonSalir.Location = new System.Drawing.Point(0, 477);
-            this.buttonSalir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonSalir.Location = new System.Drawing.Point(0, 513);
+            this.buttonSalir.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonSalir.Name = "buttonSalir";
-            this.buttonSalir.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.buttonSalir.Size = new System.Drawing.Size(180, 60);
+            this.buttonSalir.Padding = new System.Windows.Forms.Padding(23, 0, 0, 0);
+            this.buttonSalir.Size = new System.Drawing.Size(210, 65);
             this.buttonSalir.TabIndex = 9;
             this.buttonSalir.Text = "Salir";
             this.buttonSalir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -107,16 +95,15 @@
             // 
             // buttonCategorias
             // 
-            this.buttonCategorias.Dock = System.Windows.Forms.DockStyle.Top;
             this.buttonCategorias.FlatAppearance.BorderSize = 0;
             this.buttonCategorias.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonCategorias.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCategorias.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonCategorias.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.buttonCategorias.Location = new System.Drawing.Point(0, 200);
-            this.buttonCategorias.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonCategorias.Location = new System.Drawing.Point(0, 323);
+            this.buttonCategorias.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonCategorias.Name = "buttonCategorias";
-            this.buttonCategorias.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.buttonCategorias.Size = new System.Drawing.Size(180, 60);
+            this.buttonCategorias.Padding = new System.Windows.Forms.Padding(23, 0, 0, 0);
+            this.buttonCategorias.Size = new System.Drawing.Size(210, 65);
             this.buttonCategorias.TabIndex = 8;
             this.buttonCategorias.Text = "Categorías";
             this.buttonCategorias.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -124,16 +111,15 @@
             // 
             // buttonMarcas
             // 
-            this.buttonMarcas.Dock = System.Windows.Forms.DockStyle.Top;
             this.buttonMarcas.FlatAppearance.BorderSize = 0;
             this.buttonMarcas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonMarcas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMarcas.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonMarcas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.buttonMarcas.Location = new System.Drawing.Point(0, 140);
-            this.buttonMarcas.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonMarcas.Location = new System.Drawing.Point(0, 251);
+            this.buttonMarcas.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonMarcas.Name = "buttonMarcas";
-            this.buttonMarcas.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.buttonMarcas.Size = new System.Drawing.Size(180, 60);
+            this.buttonMarcas.Padding = new System.Windows.Forms.Padding(23, 0, 0, 0);
+            this.buttonMarcas.Size = new System.Drawing.Size(210, 65);
             this.buttonMarcas.TabIndex = 7;
             this.buttonMarcas.Text = "Marcas";
             this.buttonMarcas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -141,16 +127,15 @@
             // 
             // buttonArticulos
             // 
-            this.buttonArticulos.Dock = System.Windows.Forms.DockStyle.Top;
             this.buttonArticulos.FlatAppearance.BorderSize = 0;
             this.buttonArticulos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonArticulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonArticulos.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonArticulos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.buttonArticulos.Location = new System.Drawing.Point(0, 80);
-            this.buttonArticulos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonArticulos.Location = new System.Drawing.Point(0, 177);
+            this.buttonArticulos.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonArticulos.Name = "buttonArticulos";
-            this.buttonArticulos.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.buttonArticulos.Size = new System.Drawing.Size(180, 60);
+            this.buttonArticulos.Padding = new System.Windows.Forms.Padding(23, 0, 0, 0);
+            this.buttonArticulos.Size = new System.Drawing.Size(210, 65);
             this.buttonArticulos.TabIndex = 6;
             this.buttonArticulos.Text = "Artículos";
             this.buttonArticulos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -160,57 +145,78 @@
             // 
             this.panelLogo.BackColor = System.Drawing.Color.Transparent;
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
-            this.panelLogo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Size = new System.Drawing.Size(180, 80);
+            this.panelLogo.Size = new System.Drawing.Size(210, 85);
             this.panelLogo.TabIndex = 5;
+            // 
+            // pictureBoxDecorationImage
+            // 
+            this.pictureBoxDecorationImage.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxDecorationImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBoxDecorationImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxDecorationImage.Image = global::ArticulosAppViews.Properties.Resources.logo;
+            this.pictureBoxDecorationImage.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxDecorationImage.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.pictureBoxDecorationImage.Name = "pictureBoxDecorationImage";
+            this.pictureBoxDecorationImage.Size = new System.Drawing.Size(966, 493);
+            this.pictureBoxDecorationImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxDecorationImage.TabIndex = 10;
+            this.pictureBoxDecorationImage.TabStop = false;
+            this.pictureBoxDecorationImage.UseWaitCursor = true;
             // 
             // panelWindowTitle
             // 
-            this.panelWindowTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(119)))), ((int)(((byte)(146)))));
+            this.panelWindowTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(133)))), ((int)(((byte)(255)))));
             this.panelWindowTitle.Controls.Add(this.labelWindowTitle);
             this.panelWindowTitle.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelWindowTitle.Location = new System.Drawing.Point(180, 0);
-            this.panelWindowTitle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelWindowTitle.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelWindowTitle.Location = new System.Drawing.Point(210, 0);
+            this.panelWindowTitle.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.panelWindowTitle.Name = "panelWindowTitle";
-            this.panelWindowTitle.Size = new System.Drawing.Size(828, 80);
+            this.panelWindowTitle.Size = new System.Drawing.Size(966, 85);
             this.panelWindowTitle.TabIndex = 1;
             // 
             // labelWindowTitle
             // 
-            this.labelWindowTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelWindowTitle.AutoSize = true;
-            this.labelWindowTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWindowTitle.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelWindowTitle.ForeColor = System.Drawing.Color.White;
-            this.labelWindowTitle.Location = new System.Drawing.Point(346, 27);
+            this.labelWindowTitle.Location = new System.Drawing.Point(22, 29);
+            this.labelWindowTitle.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelWindowTitle.Name = "labelWindowTitle";
-            this.labelWindowTitle.Size = new System.Drawing.Size(78, 26);
+            this.labelWindowTitle.Size = new System.Drawing.Size(76, 25);
             this.labelWindowTitle.TabIndex = 0;
             this.labelWindowTitle.Text = "HOME";
             this.labelWindowTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panelWindows
             // 
-            this.panelWindows.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(124)))), ((int)(((byte)(158)))));
+            this.panelWindows.BackColor = System.Drawing.Color.Transparent;
+            this.panelWindows.Controls.Add(this.pictureBoxDecorationImage);
             this.panelWindows.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelWindows.Location = new System.Drawing.Point(180, 80);
-            this.panelWindows.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelWindows.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelWindows.Location = new System.Drawing.Point(210, 85);
+            this.panelWindows.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.panelWindows.Name = "panelWindows";
-            this.panelWindows.Size = new System.Drawing.Size(828, 457);
+            this.panelWindows.Size = new System.Drawing.Size(966, 493);
             this.panelWindows.TabIndex = 2;
             // 
             // viewMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.ClientSize = new System.Drawing.Size(1008, 537);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(1176, 578);
             this.Controls.Add(this.panelWindows);
             this.Controls.Add(this.panelWindowTitle);
             this.Controls.Add(this.panelMenu);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.MinimumSize = new System.Drawing.Size(1024, 576);
+            this.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MinimumSize = new System.Drawing.Size(1126, 617);
             this.Name = "viewMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Artículos App";
@@ -219,6 +225,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDecorationImage)).EndInit();
             this.panelWindowTitle.ResumeLayout(false);
             this.panelWindowTitle.PerformLayout();
+            this.panelWindows.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }

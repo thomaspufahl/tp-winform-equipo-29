f8e0ca25dce76c8a93ec98d9b677092984f21fc8
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-09-11 13:43:47 -0300

    Agregué el modelo Categoria

---
+++ ArticulosAppModels/Categoria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticulosAppModels
+{
+    public class Categoria
+    {
+        public int Id { get; }
+        public string Description { get; set; }
+
+        public Categoria()
+        {
+            Id = -1;
+            Description = string.Empty;
+        }
+
+        public Categoria(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}

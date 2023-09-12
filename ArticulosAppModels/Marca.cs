678e748924362e1a8feeeba0c087d964d67d8651
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-09-12 18:59:58 -0300

    Agregué el modelo Marca

---
+++ ArticulosAppModels/Marca.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticulosAppModels
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public Marca()
+        {
+            Id = -1;
+            Description = string.Empty;
+        }
+
+        public Marca(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}

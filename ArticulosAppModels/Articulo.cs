f63ee9d1583c323bb2e4acc1b96fd45b7e02ae22
coutnico <117245006+coutnico@users.noreply.github.com>
2023-09-12 18:02:25 -0300

    ahora si perdon

--- ArticulosAppModels/Articulo.cs
+++ ArticulosAppModels/Articulo.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ArticulosAppModels
 {
-    internal class Articulo
+    public class Articulo
     {
+        public int CodigoArticulo { get; }
+        public string NombreArticulo { get; set; }
+        public string Descripcion { get; set; }
+        //public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public Imagen Imagen { get; set;}
+        public float Precio { get; set; }
+
+        public Articulo()
+        {
+            CodigoArticulo = -1;
+            NombreArticulo = string.Empty;
+            Descripcion = string.Empty;
+            //Marca = new Marca();
+            Categoria = new Categoria();
+            Imagen = new Imagen();
+            Precio = 0;
+        }
+        public Articulo(int id, string nombre, string descripcion, Categoria categoria, float precio)
+        {
+            CodigoArticulo = id;
+            NombreArticulo = nombre;
+            Descripcion = descripcion;
+            //Marca = new Marca();
+            Categoria = categoria;
+            Imagen = new Imagen();
+            Precio = precio;
+        }
+
+
+
+
     }
 }

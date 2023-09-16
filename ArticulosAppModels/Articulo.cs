770b4d9d67a711352320792b2302bd26c95633b3
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-09-16 19:30:54 -0300

    Actualicé el modelo Articulo

--- ArticulosAppModels/Articulo.cs
+++ ArticulosAppModels/Articulo.cs
@@ -14,30 +14,30 @@ namespace ArticulosAppModels
         public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-        public int IdMarca { get; set; }
-        public int IdCategoria { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
         public decimal Precio { get; set; }
 
         public Articulo() {}
 
-        public Articulo(string codigo, string nombre, string descripcion, int idMarca, int idCategoria, decimal precio)
+        public Articulo(string codigo, string nombre, string descripcion, Marca marca, Categoria categoria, decimal precio)
         {
             Codigo = codigo;
             Nombre = nombre;
             Descripcion = descripcion;
-            IdMarca = idMarca;
-            IdCategoria = idCategoria;
+            Marca = marca;
+            Categoria = categoria;
             Precio = precio;
         }
 
-        public Articulo(int id, string codigo, string nombre, string descripcion, int idMarca, int idCategoria, decimal precio)
+        public Articulo(int id, string codigo, string nombre, string descripcion, Marca marca, Categoria categoria, decimal precio)
         {
             Id = id;
             Codigo = codigo;
             Nombre = nombre;
             Descripcion = descripcion;
-            IdMarca = idMarca;
-            IdCategoria = idCategoria;
+            Marca = marca;
+            Categoria = categoria;
             Precio = precio;
         }
 

18f43470a0e897e968554a8c8fba180a1a8cd0d8
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-09-16 13:37:30 -0300

    Agregué la clase AccesoDatos para utilizar la base de datos

---
+++ ArticulosAppServices/AccesoDatos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticulosAppServices
+{
+    internal class AccesoDatos
+    {
+        private SqlConnection conex;
+        private SqlCommand command;
+        private SqlDataReader reader;
+
+        public SqlDataReader Reader { get { return reader; } }
+
+        public AccesoDatos()
+        {
+            conex = new SqlConnection();
+            command = new SqlCommand();
+
+            conex.ConnectionString = ConfigurationManager.AppSettings["DevPufahlConnectionString"];
+        }
+
+        public void setQuery(string query)
+        {
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = query;
+        }
+
+        public void setParams(string name, object value)
+        {
+            command.Parameters.AddWithValue(name, value);
+        }
+
+        public void executeSelectionQuery()
+        {
+            command.Connection = conex;
+
+            try
+            {
+                conex.Open();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                conex.Close();
+                throw ex;
+            }
+
+        }
+        public void executeActionQuery()
+        {
+            command.Connection = conex;
+
+            try
+            {
+                conex.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+        public void closeConnection()
+        {
+            if (reader != null && !reader.IsClosed)
+                reader.Close();
+
+            conex.Close();
+        }
+    }
+}

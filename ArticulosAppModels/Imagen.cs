82e124166eac54bb10cc67b4b0b8f38b31eb2003
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-09-17 08:45:12 -0300

    Actualicé el modelo Imagen

--- ArticulosAppModels/Imagen.cs
+++ ArticulosAppModels/Imagen.cs
@@ -8,23 +8,28 @@ namespace ArticulosAppModels
 {
     public class Imagen
     {
-        public int Id { get; set; }
+        public int Id { get; }
         public int IdArticulo { get; set; }
         public string UrlImagen { get; set; }
 
 
-        public Imagen()
+        public Imagen() {}
+        public Imagen(int idArticulo, string urlImagen)
         {
-            Id = -1;
-            IdArticulo = -1;
-            UrlImagen = "https://img1.picmix.com/output/stamp/normal/1/6/2/9/1279261_0151e.png";
+            IdArticulo = idArticulo;
+            UrlImagen = urlImagen;
         }
-        public Imagen(int id, int idart, string url)
+        public Imagen(int id, int idArticulo, string urlImagen)
         {
             Id = id;
-            IdArticulo = idart;
-            UrlImagen = url;
+            IdArticulo = idArticulo;
+            UrlImagen = urlImagen;
         }
 
+
+        public override string ToString()
+        {
+            return UrlImagen;
+        }
     }
 }

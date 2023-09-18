7c6220dbf6d413eddb742190c6653d898e423923
coutnico <nicoo_rodrii@hotmail.com>
2023-09-17 21:58:17 -0300

    Cambié el nombre de la columna descripcion en marca y categoria

--- ArticulosAppModels/Categoria.cs
+++ ArticulosAppModels/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace ArticulosAppModels
     public class Categoria
     {
         public int Id { get; }
+
+        [DisplayName("Descripción")]
         public string Description { get; set; }
 
         public Categoria() { }
--- ArticulosAppModels/Marca.cs
+++ ArticulosAppModels/Marca.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace ArticulosAppModels
     public class Marca
     {
         public int Id { get; set; }
+
+        [DisplayName("Descripción")]
         public string Description { get; set; }
 
         public Marca() { }
